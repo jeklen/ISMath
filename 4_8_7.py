bc732ce47cf5a1fe3f416c7f1a88a62d4f2b51a0
Jek <zhql0907@outlook.com>
2017-04-24 19:52:11 +0800

    fix mistakes

--- 4_8_7.py
+++ 4_8_7.py
@@ -6,26 +6,12 @@ m = int((x*x + y*y) / p)
 u = int(x % m)
 v = 1
 while(m!=1):
-    xtmp = x
-    ytmp = y
-    x = int((u*xtmp + v*ytmp) / m)
-    y = int((u*ytmp - v*xtmp) / m)
-    m = int((x*x + y*y) / p)
-    u = int(x % m)
-    v = int(y % m)
-	rpint(x, y)
-	w(2, int((p-1)/4)) % p
-	y = 1
+	xtmp = x
+	ytmp = y
+ 	x = int((u*xtmp + v*ytmp) / m)
+	y = int((u*ytmp - v*xtmp) / m)
 	m = int((x*x + y*y) / p)
-	u = int(x % m)
-	v = 1
-	while(m!=1):
-	    xtmp = x
-	    ytmp = y
- 	    x = int((u*xtmp + v*ytmp) / m)
-	    y = int((u*ytmp - v*xtmp) / m)
-	    m = int((x*x + y*y) / p)
-            u = int(x % m)
-            v = int(y % m)
-	    print(x, y)
+        u = int(x % m)
+        v = int(y % m)
+print(x, y)
 
